f19fc3998254ff67a7f51dbdf056e03211861836
mrmauricio <mendes-mauricio@outlook.com>
2019-10-17 20:03:50 +0100

    fetched pokemon data from pokeapi

--- src/app/pages/Main/main.component.ts
+++ src/app/pages/Main/main.component.ts
@@ -9,4 +9,26 @@ import { Pokemon } from "../../classes/pokemon";
     templateUrl: "./main.component.html",
     styleUrls: ["./main.component.scss"]
 })
-export class MainComponent implements OnInit {}
+export class MainComponent implements OnInit {
+    pokemon: Pokemon[] = [];
+    availablePokemon: number[] = [6, 9];
+
+    isLoading: boolean = false;
+    error: boolean = false;
+
+    constructor(private pokemonService: PokemonService) {}
+
+    ngOnInit() {
+        this.loadPokemon(this.availablePokemon);
+    }
+
+    async loadPokemon(availablePokemon: number[]): Promise<void> {
+        this.isLoading = true;
+
+        const data = await this.pokemonService.getPokemon(availablePokemon);
+
+        this.pokemon = data;
+
+        console.log(this.pokemon);
+    }
+}
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -8,4 +8,27 @@ import { Pokemon } from "../classes/pokemon";
 })
 export class PokemonService {
     constructor(private http: HttpClient) {}
+
+    private baseUrl: string = "https://pokeapi.co/api/v2/pokemon";
+
+    async getPokemon(idArray: number[]) {
+        const pokeArray: Promise<Pokemon[]> = Promise.all(
+            idArray.map(async (id) => {
+                const data: any = await this.http
+                    .get(`${this.baseUrl}/${id}`)
+                    .toPromise();
+
+                console.log(data);
+
+                const pokemon: Pokemon = {
+                    id: data.id,
+                    sprite: data.sprites.back_default,
+                    name: data.name
+                };
+                return pokemon;
+            })
+        );
+
+        return pokeArray;
+    }
 }
