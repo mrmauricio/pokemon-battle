8def539191dd7f05d97d5c506ea9b13fb9c64710
mrmauricio <mendes-mauricio@outlook.com>
2019-10-22 23:01:20 +0100

    added available moves to fighters

---
+++ src/app/utils/movesIdList.ts
@@ -0,0 +1,196 @@
+import { MoveList } from "./../classes/pokemon";
+
+export const movesIdList: MoveList[] = [
+    {
+        pokemonId: 3,
+        movesIdList: [412, 188, 89, 36]
+        // energy ball - 90 - GRASS
+        // sludge bomb - 90 - POISON
+        // earthquake - 100 - GROUND
+        // take down - 90 - NORMAL
+    },
+    {
+        pokemonId: 6,
+        movesIdList: [53, 211, 337, 403]
+        // flamethrower - 90 - FIRE
+        // steel wing - 70 - STEEL
+        // dragon claw - 80 - DRAGON
+        // air slash - 75 - FLYING
+    },
+    {
+        pokemonId: 9,
+        movesIdList: [57, 89, 58, 339]
+        // surf - 90 - WATER
+        // earthquake - 100 - GROUND
+        // ice beam - 90 - ICE
+        // dark pulse - 80 - DARK
+    },
+    {
+        pokemonId: 12,
+        movesIdList: [403, 94, 405, 247]
+        // air slash - 75 - FLYING
+        // psychic - 90 - PSYCHIC
+        // bug buzz - 90 - BUG
+        // shadow ball - 80 - GHOST
+    },
+    {
+        pokemonId: 18,
+        movesIdList: [403, 211, 36, 369]
+        // air slash - 75 - FLYING
+        // steel wing - 70 - STEEL
+        // take down - 90 - NORMAL
+        // u turn - 70 - BUG
+    },
+    {
+        pokemonId: 31,
+        movesIdList: [89, 188, 58, 280]
+        // earthquake - 100 - GROUND
+        // sludge bomb - 90 - POISON
+        // ice beam - 90 - ICE
+        // brick break - 75 - FIGHTING
+    },
+    {
+        pokemonId: 34,
+        movesIdList: [89, 188, 58, 280]
+        // earthquake - 100 - GROUND
+        // sludge bomb - 90 - POISON
+        // ice beam - 90 - ICE
+        // brick break - 75 - FIGHTING
+    },
+    {
+        pokemonId: 38,
+        movesIdList: [53, 339, 412, 326]
+        // flamethrower - 90 - FIRE
+        // dark pulse - 80 - DARK
+        // energy ball - 90 - GRASS
+        // extrasensory - 80 - PSYCHIC
+    },
+    {
+        pokemonId: 45,
+        movesIdList: [412, 605, 188, 36]
+        // energy ball - 90 - GRASS
+        // dazzling gleam - 80 - FAIRY
+        // sludge bomb - 90 - POISON
+        // take down - 90 - NORMAL
+    },
+    {
+        pokemonId: 65,
+        movesIdList: [94, 247, 412, 605]
+        // psychic - 90 - PSYCHIC
+        // shadow ball - 80 - GHOST
+        // energy ball - 90 - GRASS
+        // dazzling gleam - 80 - FAIRY
+    },
+    {
+        pokemonId: 68,
+        movesIdList: [280, 53, 89, 157]
+        // brick break - 75 - FIGHTING
+        // flamethrower - 90 - FIRE
+        // earthquake - 100 - GROUND
+        // rock slide - 75 - ROCK
+    },
+    {
+        pokemonId: 76,
+        movesIdList: [89, 157, 53, 36]
+        // earthquake - 100 - GROUND
+        // rock slide - 75 - ROCK
+        // flamethrower - 90 - FIRE
+        // take down - 90 - NORMAL
+    },
+    {
+        pokemonId: 82,
+        movesIdList: [85, 430, 36, 33]
+        // thunderbolt - 90 - ELECTRIC
+        // flash cannon - 80 - STEEL
+        // take down - 90 - NORMAL
+        // tackle - 40 - NORMAL
+    },
+    {
+        pokemonId: 91,
+        movesIdList: [58, 57, 398, 36]
+        // ice beam - 90 - ICE
+        // surf - 90 - WATER
+        // poison jab - 80 - POISON
+        // take down - 90 - NORMAL
+    },
+    {
+        pokemonId: 94,
+        movesIdList: [247, 85, 188, 280]
+        // shadow ball - 80 - GHOST
+        // thunderbolt - 90 - ELECTRIC
+        // sludge bomb - 90 - POISON
+        // brick break - 75 - FIGHTING
+    },
+    {
+        pokemonId: 130,
+        movesIdList: [57, 53, 89, 85]
+        // surf - 90 - WATER
+        // flamethrower - 90 - FIRE
+        // earthquake - 100 - GROUND
+        // thunderbolt - 90 - ELECTRIC
+    },
+    {
+        pokemonId: 131,
+        movesIdList: [58, 57, 85, 94]
+        // ice beam - 90 - ICE
+        // surf - 90 - WATER
+        // thunderbolt - 90 - ELECTRIC
+        // psychic - 90 - PSYCHIC
+    },
+    {
+        pokemonId: 142,
+        movesIdList: [157, 337, 53, 332]
+        // rock slide - 75 - ROCK
+        // dragon claw - 80 - DRAGON
+        // flamethrower - 90 - FIRE
+        // aerial ace - 60 - FLYING
+    },
+    {
+        pokemonId: 144,
+        movesIdList: [58, 332, 211, 369]
+        // ice beam - 90 - ICE
+        // aerial ace - 60 - FLYING
+        // steel wing - 70 - STEEL
+        // u turn - 70 - BUG
+    },
+    {
+        pokemonId: 145,
+        movesIdList: [85, 332, 211, 369]
+        // thunderbolt - 90 - ELECTRIC
+        // aerial ace - 60 - FLYING
+        // steel wing - 70 - STEEL
+        // u turn - 70 - BUG
+    },
+    {
+        pokemonId: 146,
+        movesIdList: [53, 332, 211, 369]
+        // flamethrower - 90 - FIRE
+        // aerial ace - 60 - FLYING
+        // steel wing - 70 - STEEL
+        // u turn - 70 - BUG
+    },
+    {
+        pokemonId: 149,
+        movesIdList: [337, 57, 85, 89]
+        // dragon claw - 80 - DRAGON
+        // surf - 90 - WATER
+        // thunderbolt - 90 - ELECTRIC
+        // earthquake - 100 - GROUND
+    },
+    {
+        pokemonId: 150,
+        movesIdList: [94, 247, 53, 85]
+        // psychic - 90 - PSYCHIC
+        // shadow ball - 80 - GHOST
+        // flamethrower - 90 - FIRE
+        // thunderbolt - 90 - ELECTRIC
+    },
+    {
+        pokemonId: 151,
+        movesIdList: [94, 58, 247, 89]
+        // psychic - 90 - PSYCHIC
+        // ice beam - 90 - ICE
+        // shadow ball - 80 - GHOST
+        // earthquake - 100 - GROUND
+    }
+];
