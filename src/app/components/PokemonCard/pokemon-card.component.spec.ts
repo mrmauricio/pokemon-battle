b3fd92de2a372b926cf076253114c9a35c953b9a
mrmauricio <mendes-mauricio@outlook.com>
2019-12-02 18:13:32 +0000

    added tests to PokemonCard Component

--- src/app/components/PokemonCard/pokemon-card.component.spec.ts
+++ src/app/components/PokemonCard/pokemon-card.component.spec.ts
@@ -1,26 +1,76 @@
-/*import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
-import { PokemonCardComponent } from './pokemon-card.component';
+import { PokemonCardComponent } from "./pokemon-card.component";
 
-describe('PokemonCardComponent', () => {
-  let component: PokemonCardComponent;
-  let fixture: ComponentFixture<PokemonCardComponent>;
+import { mockPokemonPreview } from "../../mocks/mockPokemonPreview";
+import { By } from "@angular/platform-browser";
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PokemonCardComponent ]
-    })
-    .compileComponents();
-  }));
+describe("-------------- (component) PokemonCardComponent --------------", () => {
+    let fixture: ComponentFixture<PokemonCardComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PokemonCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [PokemonCardComponent],
+            imports: [RouterTestingModule]
+        });
+        fixture = TestBed.createComponent(PokemonCardComponent);
+        fixture.componentInstance.pokemon = mockPokemonPreview;
+        fixture.componentInstance.trophyAmount = 0;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    describe(":", () => {
+        it("should display this pokemon's id and name inside a h2 tag", () => {
+            let h2Text = fixture.nativeElement.querySelector("h2").textContent;
+
+            expect(h2Text).toEqual("#6 Charizard");
+        });
+
+        it("should display an anchor to this pokemon's detail page", () => {
+            let href = fixture.debugElement
+                .query(By.css("a"))
+                .nativeElement.getAttribute("href");
+
+            expect(href).toEqual("/pokemon/6");
+        });
+
+        it("should display this pokemon's sprite on a img tag", () => {
+            let src = fixture.nativeElement
+                .querySelector("img#pokemon")
+                .getAttribute("src");
+
+            expect(src).toEqual(fixture.componentInstance.pokemon.sprite);
+        });
+
+        it("should display pokemon's shiny sprite on a img tag if this pokemon won all 6 trophies", () => {
+            fixture.componentInstance.trophyAmount = 6;
+            fixture.detectChanges();
+
+            let src = fixture.nativeElement
+                .querySelector("img#pokemon")
+                .getAttribute("src");
+
+            expect(src).toEqual(fixture.componentInstance.pokemon.spriteShiny);
+        });
+
+        it("should display a div with this pokemon's types", () => {
+            let divText = fixture.nativeElement.querySelector("div")
+                .textContent;
+
+            expect(divText).toEqual(" FIRE / FLYING ");
+        });
+
+        it("should display a trophy img tag for each trophy this pokemon won", () => {
+            fixture.componentInstance.trophyAmount = 6;
+            fixture.componentInstance.ngOnInit();
+            fixture.detectChanges();
+
+            let trophyImgs = fixture.nativeElement.querySelectorAll(
+                "img.trophy"
+            );
+
+            expect(trophyImgs.length).toBe(6);
+        });
+    });
 });
-*/
---
+++ src/app/mocks/mockPokemonPreview.ts
@@ -0,0 +1,9 @@
+export const mockPokemonPreview = {
+    id: 6,
+    sprite:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+    spriteShiny:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/6.png",
+    name: "charizard",
+    types: ["fire", "flying"]
+};
