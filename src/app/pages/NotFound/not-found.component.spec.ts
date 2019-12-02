ac691517054ad8029fc8b103e7941a2dce84757a
mrmauricio <mendes-mauricio@outlook.com>
2019-12-02 20:53:39 +0000

    added tests to NotFound Page

--- src/app/pages/NotFound/not-found.component.spec.ts
+++ src/app/pages/NotFound/not-found.component.spec.ts
@@ -1,25 +1,36 @@
-//import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-//import { NotFoundComponent } from './not-found.component';
-//
-//describe('NotFoundComponent', () => {
-//  let component: NotFoundComponent;
-//  let fixture: ComponentFixture<NotFoundComponent>;
-//
-//  beforeEach(async(() => {
-//    TestBed.configureTestingModule({
-//      declarations: [ NotFoundComponent ]
-//    })
-//    .compileComponents();
-//  }));
-//
-//  beforeEach(() => {
-//    fixture = TestBed.createComponent(NotFoundComponent);
-//    component = fixture.componentInstance;
-//    fixture.detectChanges();
-//  });
-//
-//  it('should create', () => {
-//    expect(component).toBeTruthy();
-//  });
-//});
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
+
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
+import { NotFoundComponent } from "./not-found.component";
+
+describe("-------------- (component) NotFoundComponent --------------", () => {
+    let fixture: ComponentFixture<NotFoundComponent>;
+    let mockRouter = {
+        navigate: jasmine.createSpy("navigate")
+    };
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [NotFoundComponent],
+            imports: [FontAwesomeModule],
+            providers: [{ provide: Router, useValue: mockRouter }]
+        });
+
+        fixture = TestBed.createComponent(NotFoundComponent);
+        fixture.detectChanges();
+    });
+
+    describe(":", () => {
+        it("should display a not-found image", () => {
+            expect(fixture.nativeElement.querySelector("img")).toBeTruthy();
+        });
+
+        it("should have a button that redirect to home page when clicked", () => {
+            fixture.nativeElement.querySelector("button").click();
+
+            expect(mockRouter.navigate).toHaveBeenCalledWith(["/"]);
+        });
+    });
+});
